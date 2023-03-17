1380705afcaeab3bf96993963a66ccf97125a329
navneet2508 <navneety250801@gmail.com>
2023-03-18 02:37:16 +0530

    length of string

---
+++ simple-data-structure/Strings/lengthofstring.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int lengthofstring(char ch[])
+{
+    int count = 0;
+    for (int i = 0; ch[i] != '\0'; i++)
+    {
+        count++;
+    }
+    return count;
+}
+int main()
+{
+    char ch[20];
+    cout << "Enter your name" << endl;
+    cin >> ch;
+    int ans = lengthofstring(ch);
+    cout << "Length of your name is " << ans << endl;
+}
