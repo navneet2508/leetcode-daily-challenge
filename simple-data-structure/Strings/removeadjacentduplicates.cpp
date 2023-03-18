41b7ac4b1aeca02288e2f73d42c03f9269f18f75
navneet2508 <navneety250801@gmail.com>
2023-03-18 16:23:16 +0530

    remove adjacent duplicates

---
+++ simple-data-structure/Strings/removeadjacentduplicates.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+string removeadjduplicates(string s)
+{
+    string ans = "";
+    int i = 0;
+    while (i < s.length())
+    {
+        if (ans.length() > 0 && ans[ans.length() - 1] == s[i])
+            ans.pop_back();
+        else
+            ans.push_back(s[i]);
+        i++;
+    }
+    return ans;
+}
+int main()
+{
+    string s;
+    cout << "Enter ur string" << endl;
+    getline(cin, s);
+    cout << "Your string after removing adjacent duplicates is " << removeadjduplicates(s);
+}
