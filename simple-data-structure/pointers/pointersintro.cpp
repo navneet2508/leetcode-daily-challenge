51081c7e8e534fe3f894e43edfd71181465bc786
navneet2508 <navneety250801@gmail.com>
2023-03-20 15:14:51 +0530

    pointers introduction

---
+++ simple-data-structure/pointers/pointersintro.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num = 5;
+    int *p = &num;
+    cout << "Number is " << num << endl;
+    cout << "Value pointed by *p is " << *p << endl;
+    cout << "Address pointed by p is " << p << endl;
+    num++;
+    cout << "Value pointed by *p after increment in num is " << *p << endl;
+    cout << "Address of num after increment is " << p << endl;
+
+    double d = 4.2;
+    // Pointer type is same as of variable type
+    double *ptr = &d;
+
+    // As variable name denotes different variables and pointer denotes their address.
+    // The size of different variables may differ but size of pointer will be same
+    cout << "Size of num variable is " << sizeof(num) << endl;
+    cout << "Size of pointer of num variable(int type) is " << sizeof(p) << endl;
+    cout << "Size of d variable is " << sizeof(d) << endl;
+    cout << "Size of pointer of d variable(double type) is " << sizeof(ptr) << endl;
+}
