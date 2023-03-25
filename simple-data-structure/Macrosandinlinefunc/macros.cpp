7fb096bb432319790bfe04a93d0ce052962bb0a9
navneet2508 <navneety250801@gmail.com>
2023-03-25 20:20:40 +0530

    added comments to macros

--- simple-data-structure/Macrosandinlinefunc/macros.cpp
+++ simple-data-structure/Macrosandinlinefunc/macros.cpp
@@ -1,11 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define PI 3.14
+#define PI 3.14 // Using macro function will not take any space.
 int main()
 {
     int r = 5;
+
+    // It will take some space which will decrease speed in very small amount
     // double pi=3.14
     // double area=pi*r*r;
+
     double area = PI * r * r;
     cout << "Area of circle is -> " << area << endl;
 }
